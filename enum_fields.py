967a991e26c6bab6f3728d1028f917dacea4e476
windyludev <windyludev@outlook.com>
2019-05-10 10:17:39 -0400

    Separate enum_fields in its own file

---
+++ enum_fields.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Field(Enum):
+    DATE = "date"
+    NAME = "name"
+    NOTES = "notes"
+    TASK_NAME = "task_name"
+    TIME_WORK = "time_work"
+    OTHERS = "others"
